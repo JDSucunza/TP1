d16e62c6529243be01e7e659a6ec6fe816887dda
JDSucunza <jdsucunza@gmail.com>
2021-09-16 04:55:10 -0300

    Create MoveEnemy.cs
    Movement Enemy const.

---
+++ Assets/Scripts/MoveEnemy.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveEnemy : MonoBehaviour
+{
+    
+    public int speedVert;
+    public int speedHor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       this.transform.Translate (speedHor * Time.deltaTime, 0, speedVert * Time.deltaTime); 
+    }
+}
