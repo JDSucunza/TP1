6f32dab4a1b268671f5285c4fc310f0b6f825cf2
JDSucunza <jdsucunza@gmail.com>
2021-09-16 04:55:35 -0300

    Create Life.cs
    Set life to player.

---
+++ Assets/Scripts/Life.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class health : MonoBehaviour
+{
+    
+    public float cant;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (cant <= 0){
+            Destroy (gameObject);
+        }    
+    }
+
+    void Life (){
+        this.cant = cant;
+    }
+}
