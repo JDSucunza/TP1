06dbeb55787d7e96049e31dc70b312d447852149
JDSucunza <jdsucunza@gmail.com>
2021-10-01 19:23:58 -0300

    Create EnemyManager.cs

---
+++ Assets/Scripts/EnemyManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+    
+    public int dead = 0;
+
+    public int countEnemy = 0;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
