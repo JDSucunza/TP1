c1ae70464f33eee79ffba31cc11cf6420554b006
JDSucunza <jdsucunza@gmail.com>
2021-10-01 19:24:17 -0300

    Create Damage.cs

---
+++ Assets/Scripts/Damage.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Damage : MonoBehaviour
+{
+    
+    public int damage;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnColiisionEnter (Collision c ){
+        Vida vidaDelObjeto = c.gameObject.GetComponent <Vida>();
+        if (vidaDelObjeto.vida > damage){
+            vidaDelObjeto.vida = vidaDelObjeto.vida - damage;
+        }   else {
+            Destroy (c.gameObject);
+        }
+    }      
+                                                               
+}
