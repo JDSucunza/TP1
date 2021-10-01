51d1cb56800296d37efdd468d9a40f5f1f96e125
JDSucunza <jdsucunza@gmail.com>
2021-10-01 19:23:30 -0300

    Create Shoot.cs

---
+++ Assets/Scripts/Shoot.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    
+    public GameObject prefabBullet;
+    public int bullets = 0;
+
+    public float sizePrefab;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown ("Fire1") && bullets > 0){
+           Instantiate (this.prefabBullet, this.transform.position + (Vector3.forward * this.sizePrefab) ,this.transform.rotation);
+            bullets = bullets - 1;
+        }
+    }
+}
