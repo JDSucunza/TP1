bdeb9b05c16c8673fb73857b8243b966a09a0c3a
JDSucunza <jdsucunza@gmail.com>
2021-10-01 19:24:20 -0300

    Create ApuntarObjetivo.cs

---
+++ Assets/Scripts/ApuntarObjetivo.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ApuntarObjetivo : MonoBehaviour
+{
+    
+    
+    private Transform player;
+    
+    // Start is called before the first frame update
+    
+     void Start()
+    {
+       // player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+       this.transform.LookAt(player);
+    }
+
+} 
