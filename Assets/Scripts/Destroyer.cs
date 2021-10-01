1e032a2d9e481f1540aa4d254b3fc1cbd499f5d6
JDSucunza <jdsucunza@gmail.com>
2021-10-01 19:24:07 -0300

    Create Destroyer.cs

---
+++ Assets/Scripts/Destroyer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Destroyer : MonoBehaviour
+{
+
+    
+   
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter (Collision c){
+       
+       if (c.gameObject == GameObject.FindWithTag ("Player")){
+        
+        Destroy (c.gameObject);
+       }
+    }
+}
