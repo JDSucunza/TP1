a62229e99d5ce9fc228ea90e7762b46d25b27e5e
JDSucunza <jdsucunza@gmail.com>
2021-10-07 03:57:13 -0300

    Create DeactiveBomb.cs

---
+++ Assets/Scripts/DeactiveBomb.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeactiveBomb : MonoBehaviour
+{
+    public bool active = true;
+
+    public GameObject player;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (player ==null){
+            Debug.Log ("Player is not defined for DeactiveBomb");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter (Collision c){
+        active = false;
+    }
+
+}
