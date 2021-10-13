cd3a0bab1d6b317ed971eaa0b5e378305f2789d2
JDSucunza <jdsucunza@gmail.com>
2021-10-13 19:47:52 -0300

    Create AtRange.cs

---
+++ Assets/Scripts/Manager/AtRange.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AtRange : MonoBehaviour
+{
+    public GameObject player;
+    public float rango;
+
+    private void Awake(){
+        this.player = GameObject.FindWithTag("Player");
+        if(this.player == null){
+            Debug.Log("Player is null in EnemyManager");
+        }
+    }
+
+    void update (){
+        this.JugadorEnRango ();
+    }
+    public bool JugadorEnRango(){
+        return Vector3.Distance(this.transform.position, this.player.transform.position) < this.rango;
+    }
+}
