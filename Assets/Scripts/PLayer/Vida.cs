ffe2cfad3583a957dba3dc5e12f1b4b2f93afef9
JDSucunza <jdsucunza@gmail.com>
2021-10-13 19:46:30 -0300

    Create Vida.cs

---
+++ Assets/Scripts/PLayer/Vida.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vida : MonoBehaviour
+{
+    
+    public int vida;
+
+    private GameObject enemy;
+    
+    
+
+    public bool dead = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.Morir ();
+    }
+
+    // ARREGLAR !!!
+    
+    void OnCollisionEnter (Collision c){
+        
+        if (c.gameObject.CompareTag ("Enemy")){
+        Damage enemy = c.gameObject.GetComponent <Damage>();
+        vida = vida - enemy.damage;
+        }
+    }
+    void Morir (){
+        if (vida <= 0){
+                this.dead = true;
+                Destroy (gameObject);        
+        }
+    }
+}
+
