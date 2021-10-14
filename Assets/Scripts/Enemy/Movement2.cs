249d7f910a67af151e1fb861fb285bf7858fb8b3
JDSucunza <jdsucunza@gmail.com>
2021-10-14 17:49:21 -0300

    Create Movement2.cs

---
+++ Assets/Scripts/Enemy/Movement2.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement2 : MonoBehaviour
+{
+    
+    public int speedH;
+
+    public int speedV;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         this.transform.Translate (speedH * Time.deltaTime, 0, speedV * Time.deltaTime);        
+    }
+
+    void OnCollisionEnter (Collision c){
+            speedV = -speedV;
+            speedH = -speedH;
+    }
+
+}
