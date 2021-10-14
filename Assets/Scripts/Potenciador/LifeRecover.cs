c61f08bf100338ba3e8ee7bd89c5de0b890d7f7a
JDSucunza <jdsucunza@gmail.com>
2021-10-14 18:44:49 -0300

    Create LifeRecover.cs

---
+++ Assets/Scripts/Potenciador/LifeRecover.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LifeRecover : MonoBehaviour
+{
+    
+    public int lifeRec;
+    
+    private GameObject player;
+    
+    void Awake()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    
+    }
+
+    void OnTriggerEnter (Collider c){
+        if (c.gameObject.CompareTag ("Player")){
+           Vida player = c.gameObject.GetComponent <Vida> ();
+            if (player.vida < 100){
+                Destroy (this.gameObject);
+            }
+            
+            if (player.vida + lifeRec < 100){
+                    player.vida = player.vida + lifeRec;
+            } else {
+                    player.vida = 100;
+            }
+            
+        }
+    }    
+}
