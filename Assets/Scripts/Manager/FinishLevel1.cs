4b9b1bb5206c8accba2f382f2e607dfca1b27498
JDSucunza <jdsucunza@gmail.com>
2021-10-15 15:26:24 -0300

    Update FinishLevel1.cs

--- Assets/Scripts/Manager/FinishLevel1.cs
+++ Assets/Scripts/Manager/FinishLevel1.cs
@@ -6,9 +6,15 @@ public class FinishLevel1 : MonoBehaviour
 {
     public GameObject player;
 
-    
+    public GameObject finish;
+    public float timer = 0f;
+
+    public float maxTime= 500f;
     public bool gano = false;
 
+    public bool murio = false;
+
+   
      
     
     // Start is called before the first frame update
@@ -20,15 +26,30 @@ public class FinishLevel1 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (gano){
+        if (gano!= true && murio != true && timer < maxTime){
+            timer = timer + (1*Time.deltaTime);
+        }else if (murio || TiempoCumplido () ){    
             
-            SceneManager.LoadScene("3Final");
+            SceneManager.LoadScene("1Final");
+        } else {
+            SceneManager.LoadScene ("3Final");
         }
     }
 
-    void OnTriggerEnter (Collider c){
-        if (c.CompareTag ("Player")){
-            gano = true;
+    bool TiempoCumplido (){
+        return (timer >= maxTime);
+    }
+    
+    void Murio (){
+
+        Vida player = gameObject.GetComponent <Vida>();
+        if (player.dead){
+            murio = true;
         }
+    
+    }
+
+    void Gano (){
+
     }
 }
