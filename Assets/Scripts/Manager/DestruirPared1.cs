32bed804dc6ef02d0e0ee8e8f4e501fefdc972b9
JDSucunza <jdsucunza@gmail.com>
2021-10-15 15:26:29 -0300

    Create DestruirPared1.cs

---
+++ Assets/Scripts/Manager/DestruirPared1.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestruirPared1 : MonoBehaviour
+{
+    
+    public GameObject paredADestruir;
+    
+    public EnemyManager manager;
+    
+    void Awake (){
+        manager = manager.gameObject.GetComponent <EnemyManager> ();
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (manager.dead == 8){
+            Destroy (paredADestruir);
+        }
+    }
+}
