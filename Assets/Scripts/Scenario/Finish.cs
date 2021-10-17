bb6bd57d9ce30eb473a5c1aa7d2c2e2c48623e5c
JDSucunza <jdsucunza@gmail.com>
2021-10-17 18:22:17 -0300

    update

---
+++ Assets/Scripts/Scenario/Finish.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Finish : MonoBehaviour
+{
+    
+    public bool finish = false;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter (Collider c){
+        if (c.gameObject.CompareTag ("Player")){
+            finish = true;
+        }
+    }
+}
