bc6bc20a9a86d4ce82a29546c7dd3c56239346b3
JDSucunza <jdsucunza@gmail.com>
2021-10-18 17:11:56 -0300

    update

---
+++ Assets/Scripts/Enemy/ActiveEnemies.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActiveEnemies : MonoBehaviour
+{
+   
+    public GameObject [] enemigos;
+
+    public GameObject paredSector2;
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider c){
+        if(c.CompareTag("Player")){
+            
+            foreach(GameObject enemigo in enemigos){
+                enemigo.SetActive(true);
+            }
+            Destroy (paredSector2);
+            Destroy(this.gameObject);
+        }
+    }
+    
+
+    
+
+}
---
+++ Assets/Scripts/Manager/Manager4.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Manager4 : MonoBehaviour
+{
+    public float timer = 0f;
+    public float maxTime = 60f;
+    public GameObject final;
+    private GameObject player;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindWithTag ("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Termino () != true && Dead () != true && TiempoCumplido () != true){
+            timer = timer + (1 * Time.deltaTime);
+        } else if (Termino () != true && Dead () || TiempoCumplido ()){
+            SceneManager.LoadScene("4Final");
+            } else {
+                    SceneManager.LoadScene("1Final");
+            }
+    }
+
+    private bool TiempoCumplido (){
+        return (maxTime <= timer);
+    }
+    
+    private bool Termino (){
+
+        Finish esFinal = final.gameObject.GetComponent <Finish>();
+        return (esFinal.finish);
+
+    }
+
+    private bool Dead (){
+        Vida muerto = player.gameObject.GetComponent <Vida> ();
+        return (muerto.dead);
+    }
+}
